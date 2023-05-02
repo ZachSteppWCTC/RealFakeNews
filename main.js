d62ae2d17ab7bdc1f89e29f61fca8b9b11e0f88a
Zach Stepp <zstepp@my.wctc.edu>
2023-05-02 11:13:05 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("myguide");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction2() {
+    var x = document.getElementById("myguide2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
